0e7d60a307b118b3063b57f065a8ea1210198be0
549974 <549974@my.cuhsd.org>
2022-11-28 14:08:55 -0800

    Final commit

--- src/main/java/FracCalc.java
+++ src/main/java/FracCalc.java
@@ -1,33 +1,38 @@
+/** Fraction Calculator Program
+ *  w/ Simplification
+ * @author Jake Abendroth
+ * @version 1.4
+ * @since 2022-11-28
+ */
 import java.math.BigInteger;
-import java.util.*;
+import java.util.Objects;
+import java.util.Scanner;
 public class FracCalc {
-
+    /**
+     * Main method asks for String input to be passed to produceAnswer,
+     * then prints result of produceAnswer
+     */
     public static void main(String[] args) 
     {
-        // TODO: Read the input from the user and call produceAnswer with an equation
-        // Create a Scanner for user input
         Scanner input = new Scanner(System.in);
         System.out.println("Enter equation: ");
         System.out.println(produceAnswer(input.nextLine()));
         input.close();
     }
-    
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //        
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
+
+    /**
+     * Calculator processes input, and returns answer to equation
+     * @param input the (String)equation to calculate
+     * @return answer
+     */
     public static String produceAnswer(String input)
-    { 
-        // TODO: Implement this function to produce the solution to the input
+    {
         String op1 = null;
         String op2 = null;
         String operation = null;
         boolean p = false;
         boolean done = false;
+        // Separation of input into terms
         for(int i = 0; i < input.length(); i++) {
             if(input.charAt(i) == ' ' && !done) {
                 op1 = input.substring(0, i);
@@ -45,7 +50,7 @@ public class FracCalc {
         int whole1 = 0;
         int num1 = 0;
         int den1 = 1;
-        // Term 1
+        // Term 1 into individual integers
         int a = op1.indexOf("_");
         int b = op1.indexOf("/");
         if(a > -1 && b > -1) {
@@ -61,7 +66,7 @@ public class FracCalc {
         if(b == -1) {
             whole1 = Integer.parseInt(op1);
         }
-        // Term 2
+        // Term 2 into individual integers
         int x = op2.indexOf("_");
         int y = op2.indexOf("/");
         if(x > -1 && y > -1) {
@@ -94,8 +99,8 @@ public class FracCalc {
         
         int numerator;
         int denom = 1;
-
-        switch(operation) {
+        // Reading operation and calculating accordingly
+        switch(Objects.requireNonNull(operation)) {
             case "+":
                 if(den == den1) {
                     numerator = numer1 + numer2;
@@ -115,14 +120,10 @@ public class FracCalc {
                 denom = den * den1;
                 break;
             case "/":
-                boolean d = false;
-                if(numer1 < 0 && den1 != 1) {
-                    numer1 *= -1;
+                if(numer1 < 0) {
                     den1 *= -1;
-                    d = true;
                 }
-                if(numer2 < 0 && !d && den != 1) {
-                    numer2 *= -1;
+                if(numer2 < 0) {
                     den *= -1;
                 }
                 numerator = (numer1*den);
@@ -131,6 +132,7 @@ public class FracCalc {
             default:
                 return "Invalid!";
         }
+        // Improper fraction simplification
         int w = 0;
         boolean mix = false;
         if(denom < 0) {
@@ -148,6 +150,7 @@ public class FracCalc {
       BigInteger gcd = n.gcd(d);
       numerator /= gcd.intValue();
       denom /= gcd.intValue();
+      // Output forms
         if(mix && numerator != 0) {
             return w+"_"+Math.abs(numerator)+"/"+denom;
         } else if(mix && numerator == 0) {
@@ -164,7 +167,14 @@ public class FracCalc {
       } else {
           return "oops"; }
     }
-    // TODO: Fill in the space below with any helper methods that you think you will need
+
+    /**
+     * Converts a mixed number to an improper fraction
+     * @param w Whole number
+     * @param n Numerator
+     * @param d Denominator
+     * @return Improper fraction's new numerator
+     */
     public static int toImproperNum(int w, int n, int d) {
         return ((w*d)+n);
     }
